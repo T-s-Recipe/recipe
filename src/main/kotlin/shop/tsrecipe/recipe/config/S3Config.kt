ad90f90e0a4d8cdb0b0ce52b626fd01edb91079b
jonghyukleee <jh5253krkr@naver.com>
2025-09-07 21:20:07 +0900

    modify: 이미지 업로드
    - 서버에서 S3에 직접 업로드하는 방식에서 URL 기반의 클라이언트 업로드 방식으로 변경

--- src/main/kotlin/shop/tsrecipe/recipe/config/S3Config.kt
+++ src/main/kotlin/shop/tsrecipe/recipe/config/S3Config.kt
@@ -6,18 +6,18 @@ import org.springframework.context.annotation.Configuration
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
 import software.amazon.awssdk.regions.Region
-import software.amazon.awssdk.services.s3.S3AsyncClient
+import software.amazon.awssdk.services.s3.presigner.S3Presigner
 
 @Configuration
 class S3Config(
+    @Value("\${aws.region}") private val region: String,
     @Value("\${aws.access-key}") private val accessKey: String,
-    @Value("\${aws.secret-key}") private val secretKey: String,
-    @Value("\${aws.region}") private val region: String
+    @Value("\${aws.secret-key}") private val secretKey: String
 ) {
     @Bean
-    fun s3AsyncClient(): S3AsyncClient {
+    fun s3Presigner(): S3Presigner {
         val credentials = AwsBasicCredentials.create(accessKey, secretKey)
-        return S3AsyncClient.builder()
+        return S3Presigner.builder()
             .region(Region.of(region))
             .credentialsProvider(StaticCredentialsProvider.create(credentials))
             .build()
